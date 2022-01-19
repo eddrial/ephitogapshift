8e84a0e3f9af5bc6e781d00e1180c6a101c84b06
Ed Rial <eddrial@hotmail.com>
2022-01-19 16:07:15 +0100

    Dataset Working Test

--- ephitogapshift/ephitogapshift.py
+++ ephitogapshift/ephitogapshift.py
@@ -3,6 +3,7 @@ Created on 19 Jan 2022
 
 @author: oqb
 '''
+import numpy as np
 
 class UndulatorMotion(object):
     '''
@@ -10,7 +11,7 @@ class UndulatorMotion(object):
     '''
 
 
-    def __init__(self, Energy, Phase, Steps = 1):
+    def __init__(self, Energy, Phase, Steps = 1, gap_shift_table = '../bin/ba_file.idt'):
         '''
         Constructor
         '''
@@ -20,3 +21,23 @@ class UndulatorMotion(object):
         
         self.allowed_harmonics = 'exists'
         self.target_harmonic = 'exists'
+        
+        self.gap_shift_table_fname = gap_shift_table
+        
+        #self.breakpoint = 1
+        
+    def read_gap_shift_table(self):
+        self.gap_shift_table = {}
+        
+        a = np.genfromtxt(self.gap_shift_table_fname)
+        
+        #for each line
+        for line in a:
+            if 'Harmonic {}'.format(int(line[4])) in self.gap_shift_table.keys():
+                self.gap_shift_table['Harmonic {}'.format(int(line[4]))] = np.vstack([self.gap_shift_table['Harmonic {}'.format(int(line[4]))],line[:-1]])
+            else:
+                self.gap_shift_table['Harmonic {}'.format(int(line[4]))] = np.array(line[:-1])
+        
+        #check if dictionary entry exists
+        #if exists, append data to table
+        #else create dictionary entry and initialise table and set first line
--- test_ephitogapshift/test_ephitogapshift.py
+++ test_ephitogapshift/test_ephitogapshift.py
@@ -18,6 +18,8 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.eg = epgs.UndulatorMotion(500,45) #Single Point Case
+        self.eg.read_gap_shift_table() #read in gap table
+        
         self.eg1 = epgs.UndulatorMotion([500,510],30,10) #scan energy
         self.egh = epgs.UndulatorMotion([500,510],0,50) #scan energy H mode
         self.egv = epgs.UndulatorMotion([500,510],90,50) #scan energy V mode
@@ -46,17 +48,35 @@ class Test(unittest.TestCase):
     def testHasAttributeTargetHarmonic(self):
         self.assertHasAttr(self.eg,"target_harmonic")
         
-        
+    def testHasAttributeGap_shift_table_fname(self):
+        self.assertHasAttr(self.eg, "gap_shift_table_fname")
+    
     #Check Steps Parsing
     def testStepsIsOneByDefault(self):
         self.assertEqual(self.eg.steps, 1, 'Default Number Of Steps not 1')
         
     def testStepsIsInteger(self):
         self.assertIsInstance(type(self.eg.steps), int, 'Number of steps is non-integer')
+    
+    #Check Red Gap Table
+    def testHasAttributeReadGapShiftTable(self):
+        self.assertHasAttr(self.eg, 'read_gap_shift_table')    
         
+    def testReadsGapShiftTableW(self):
+        #self.eg.read_gap_shift_table()
+        self.assertIn('Harmonic 1', self.eg.gap_shift_table.keys(), 'input file is read incorrectly')
+        self.assertIn('Harmonic 3', self.eg.gap_shift_table.keys(),  'input file is read incorrectly')
+        self.assertIn('Harmonic 5', self.eg.gap_shift_table.keys(), 'input file is read incorrectly')
+        
+    def testReadGapShiftTableData(self):
+        self.assertEqual(self.eg.gap_shift_table['Harmonic 1'].__len__(), 8956, 'Input File reading incorrectly Harmonic 1')
+        self.assertEqual(self.eg.gap_shift_table['Harmonic 3'].__len__(), 16895, 'Input File reading incorrectly Harmonic 3')
+        self.assertEqual(self.eg.gap_shift_table['Harmonic 5'].__len__(), 18219, 'Input File reading incorrectly Harmonic 5')
 
 
-
+#8956 Harmonic 1
+#16895 Harmonic 3
+#18219 Harmonic 5
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
