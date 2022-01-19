f02bbd42bd2a7ae7cefbb90b9a8ef1056f8daa30
Ed Rial <eddrial@hotmail.com>
2022-01-19 09:50:54 +0100

    start class up

---
+++ ephitogapshift/ephitogapshift.py
@@ -0,0 +1,17 @@
+'''
+Created on 19 Jan 2022
+
+@author: oqb
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
