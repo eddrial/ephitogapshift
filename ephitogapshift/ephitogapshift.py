290b834641cb773bcaa1b248bc78df8278e82c4e
Ed Rial <eddrial@hotmail.com>
2022-01-26 09:43:46 +0100

    So much, including reverse triangular interpolation

--- ephitogapshift/ephitogapshift.py
+++ ephitogapshift/ephitogapshift.py
@@ -4,6 +4,9 @@ Created on 19 Jan 2022
 @author: oqb
 '''
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.tri import Triangulation,  CubicTriInterpolator, TriInterpolator, TriAnalyzer
+from shapely.geometry import LineString, point
 
 class UndulatorMotion(object):
     '''
@@ -11,22 +14,79 @@ class UndulatorMotion(object):
     '''
 
 
-    def __init__(self, Energy, Phase, Steps = 1, gap_shift_table = '../bin/ba_file.idt'):
+    def __init__(self, Energy, Phase, GSEP, Steps = 1):
         '''
         Constructor
         '''
-        self.steps = Steps
-        self.phase = Phase
-        self.energy = Energy
+        self.steps = np.array([Steps]).flatten()
+        self.phase = np.array([Phase]).flatten()
+        self.energy = np.array([Energy]).flatten()
+        self.motion = {}
+        self.GSEP = GSEP
         
-        self.allowed_harmonics = 'exists'
+        self.allowed_harmonics = np.array([])
         self.target_harmonic = 'exists'
         
-        self.gap_shift_table_fname = gap_shift_table
         
         #self.breakpoint = 1
         
-    def read_gap_shift_table(self):
+        
+    def create_motion_trajectory(self):
+        if (self.steps.__len__() == 1):
+            if self.steps == 1: 
+                if self.energy.__len__() == 1:
+                    self.energy_steps = np.array([self.energy[0]]).flatten()
+                else:
+                    self.energy_steps = np.array([self.energy]).flatten()
+                
+                if self.phase.__len__() == 1:
+                    self.phase_steps = np.array([self.phase[0]]).flatten()
+                else:
+                    self.phase_steps = np.array([self.phase]).flatten()
+                
+            elif self.steps > 1:
+                if self.energy.__len__() > 1:
+                    self.energy_steps = np.linspace(self.energy[0],self.energy[1],self.steps[0])
+                else:
+                    self.energy_steps = np.array([self.energy[0]]).flatten()
+                    
+                if self.phase.__len__() > 1:
+                    self.phase_steps = np.linspace(self.phase[0],self.phase[1],self.steps[0])
+                else:
+                    self.phase_steps = np.array([self.phase[0]]).flatten()
+        
+        elif self.steps.__len__() == 2:
+            self.energy_steps = np.linspace(self.energy[0],self.energy[1],self.steps[0])
+            self.phase_steps = np.linspace(self.phase[0],self.phase[1],self.steps[1])
+            
+            self.energy_steps, self.phase_steps = np.meshgrid(self.energy_steps, self.phase_steps)
+            
+        else:
+            self.energy_steps = np.array([self.energy]).flatten()
+            self.phase_steps = self.phase([self.phase]).flatten()
+            
+            self.energy_steps, self.phase_steps = np.meshgrid(self.energy_steps, self.phase_steps)
+        
+        
+    def check_valid_motion(self):
+        
+        for harmonic in self.GSEP.gap_shift_table:
+            #calculate gap and shift positions for energy-phase raster
+            #en, ph = np.meshgrid(self.energy, self.phase)
+            reqd_gaps = self.GSEP.inverse_lookup['{} Gap'.format(harmonic)](self.energy_steps, self.phase_steps)
+            reqd_shifts = self.GSEP.inverse_lookup['{} Shift'.format(harmonic)](self.energy_steps, self.phase_steps)
+                    
+            if np.isnan(reqd_gaps.data).any() or np.isnan(reqd_shifts.data).any():
+                pass
+            else:
+                self.allowed_harmonics = np.append(self.allowed_harmonics, int(harmonic[-1]))#append available harmonic
+                #store gap/shift pairs
+                self.motion[harmonic] = np.vstack((reqd_gaps.flatten(),reqd_shifts.flatten()))
+                
+class GapShiftEnergyPhase(object):
+    
+    def __init__(self, table_fname= '../bin/ba_file.idt'):
+        self.gap_shift_table_fname = table_fname
         self.gap_shift_table = {}
         
         a = np.genfromtxt(self.gap_shift_table_fname)
@@ -37,7 +97,32 @@ class UndulatorMotion(object):
                 self.gap_shift_table['Harmonic {}'.format(int(line[4]))] = np.vstack([self.gap_shift_table['Harmonic {}'.format(int(line[4]))],line[:-1]])
             else:
                 self.gap_shift_table['Harmonic {}'.format(int(line[4]))] = np.array(line[:-1])
+                
+    def create_inverse_lookups(self):
+        self.inverse_lookup = {}
         
-        #check if dictionary entry exists
-        #if exists, append data to table
-        #else create dictionary entry and initialise table and set first line
+        for harmonic in self.gap_shift_table:
+#            if (np.min(self.energy) > np.min(self.gap_shift_table[harmonic][:,2]) 
+#                and np.max(self.energy) < np.max(self.gap_shift_table[harmonic][:,2])):
+#                self.allowed_harmonics = np.append(self.allowed_harmonics,int(harmonic[-1]))
+                
+        #creating triangular grid, and interpolating (shift, gap)
+            #grid creation *Grid is Energy Phase Grid*
+            triObj = Triangulation(self.gap_shift_table[harmonic][:,2],self.gap_shift_table[harmonic][:,3])
+            
+            #masking z as in CubicTriInterpolator to work around an error
+            #which is due to non-singular results (i.e. the last step in a table = the penultimate step trick)
+            aa = TriInterpolator(triObj,self.gap_shift_table[harmonic][:,2])
+            aa._triangulation.get_cpp_triangulation()
+            tri_analyzer = TriAnalyzer(aa._triangulation)
+            
+            (compressed_triangles, compressed_x, compressed_y, tri_renum,
+             node_renum) = tri_analyzer._get_compressed_triangulation(True, True)
+             
+            node_mask = (node_renum == -1)
+            clean_triObj = Triangulation(self.gap_shift_table[harmonic][~node_mask,2],self.gap_shift_table[harmonic][~node_mask,3])
+            #aa._z[node_renum[~node_mask]] = aa._z   
+            #self._z = self._z[~node_mask]
+            #cubic interpolation of Gap and Shift for Energy/Phase
+            self.inverse_lookup['{} Gap'.format(harmonic)] = CubicTriInterpolator(clean_triObj,self.gap_shift_table[harmonic][~node_mask,0])
+            self.inverse_lookup['{} Shift'.format(harmonic)] = CubicTriInterpolator(clean_triObj,self.gap_shift_table[harmonic][~node_mask,1])
--- test_ephitogapshift/test_ephitogapshift.py
+++ test_ephitogapshift/test_ephitogapshift.py
@@ -4,7 +4,10 @@ Created on 19 Jan 2022
 @author: oqb
 '''
 import unittest
+import numpy as np
+from numpy import testing as npt
 import ephitogapshift as epgs
+from ephitogapshift import GapShiftEnergyPhase
 
 
 
@@ -16,20 +19,34 @@ class Test(unittest.TestCase):
         # python >=3.8 only, see below for older pythons
         self.assertTrue(testBool, msg='obj lacking an attribute. obj: {}, intendedAttr: {}'.format(obj, intendedAttr))
 
-    def setUp(self):
-        self.eg = epgs.UndulatorMotion(500,45) #Single Point Case
-        self.eg.read_gap_shift_table() #read in gap table
+    @classmethod
+    def setUpClass(cls):
+        cls.gsep = GapShiftEnergyPhase()
+        cls.gsep.create_inverse_lookups()
         
-        self.eg1 = epgs.UndulatorMotion([500,510],30,10) #scan energy
-        self.egh = epgs.UndulatorMotion([500,510],0,50) #scan energy H mode
-        self.egv = epgs.UndulatorMotion([500,510],90,50) #scan energy V mode
-        self.eg600 = epgs.UndulatorMotion(607,[-45,45],0) #scan phase at 607eV
-        self.egarb = epgs.UndulatorMotion([600,610],[-20,5],21) 
-        self.egraster = epgs.UndulatorMotion([600,610],[-45,45],[11,5])
+        cls.eg = epgs.UndulatorMotion(500,45,cls.gsep) #Single Point Case
+        cls.eg.create_motion_trajectory()
         
+        cls.eg1 = epgs.UndulatorMotion([500,510],30,cls.gsep,10) #scan energy
+        cls.eg1.create_motion_trajectory()
+        
+        cls.egh = epgs.UndulatorMotion([500,510],0,cls.gsep,50) #scan energy H mode
+        cls.egh.create_motion_trajectory()
+        
+        cls.egv = epgs.UndulatorMotion([500,510],90,cls.gsep,50) #scan energy V mode
+        cls.egv.create_motion_trajectory()
+        
+        cls.eg600 = epgs.UndulatorMotion(600,[-45,45],cls.gsep,7) #scan phase at 607eV
+        cls.eg600.create_motion_trajectory()
+        
+        cls.egarb = epgs.UndulatorMotion([600,610],[-20,5],cls.gsep,21)
+        cls.egarb.create_motion_trajectory()
+         
+        cls.egraster = epgs.UndulatorMotion([600,610],[-45,45],cls.gsep,[11,5])
+        cls.egraster.create_motion_trajectory()
 
-
-    def tearDown(self):
+    @classmethod
+    def tearDown(cls):
         pass
 
 #Check Object Attributes are created
@@ -49,34 +66,78 @@ class Test(unittest.TestCase):
         self.assertHasAttr(self.eg,"target_harmonic")
         
     def testHasAttributeGap_shift_table_fname(self):
-        self.assertHasAttr(self.eg, "gap_shift_table_fname")
+        self.assertHasAttr(self.eg, "GSEP")
     
     #Check Steps Parsing
     def testStepsIsOneByDefault(self):
         self.assertEqual(self.eg.steps, 1, 'Default Number Of Steps not 1')
-        
-    def testStepsIsInteger(self):
-        self.assertIsInstance(type(self.eg.steps), int, 'Number of steps is non-integer')
     
-    #Check Red Gap Table
-    def testHasAttributeReadGapShiftTable(self):
-        self.assertHasAttr(self.eg, 'read_gap_shift_table')    
+    def testStepsParsing_FixedEnergyFixedPhase_Energy(self):
+        npt.assert_array_equal(self.eg.energy_steps, np.array([500]),
+                                'Motion Trajectory Calculation Fail: Energy Range, Fixed Phase')
         
+    def testStepsParsing_FixedEnergyFixedPhase_Phase(self):
+        npt.assert_array_equal(self.eg.phase_steps, np.array([45]).flatten(),
+                                'Motion Trajectory Calculation Fail: Energy Range, Fixed Phase')
+        
+    def testStepsParsing_RangeEnergyFixedPhase_Energy(self):
+        npt.assert_array_equal(self.eg1.energy_steps, np.linspace(500,510,10),
+                                'Motion Trajectory Calculation Fail: Energy Range, Fixed Phase')
+        
+    def testStepsParsing_RangeEnergyFixedPhase_Phase(self):
+        npt.assert_array_equal(self.eg1.phase_steps, np.array([30]).flatten(),
+                                'Motion Trajectory Calculation Fail: Energy Range, Fixed Phase')
+    
+    def testStepsParsing_FixeedEnergyRangePhase_Energy(self):
+        npt.assert_array_equal(self.eg600.energy_steps, np.array([600]),
+                                'Motion Trajectory Calculation Fail: Energy Fixed, Phase Range')
+        
+    def testStepsParsing_FixedEnergyRangePhase_Phase(self):
+        npt.assert_array_equal(self.eg600.phase_steps, np.linspace(-45,45,7).flatten(),
+                                'Motion Trajectory Calculation Fail: Energy Fixed, Phase Range')
+        
+    def testStepsParsing_ArbitraryEnergyPhase_Energy(self):
+        npt.assert_array_equal(self.egarb.energy_steps, np.linspace(600,610,21),
+                                'Motion Trajectory Calculation Fail: Arbitrary Path')
+        
+    def testStepsParsing_ArbitraryEnergyPhase_Phase(self):
+        npt.assert_array_equal(self.egarb.phase_steps, np.linspace(-20,5,21).flatten(),
+                                'Motion Trajectory Calculation Fail: Arbitrary Path')
+    
+    def testStepsParsing_RasterEnergyPhase_Energy(self):
+        npt.assert_array_equal(self.egraster.energy_steps, np.array([[600., 601., 602., 603., 604., 605., 606., 607., 608., 609., 610.],
+                                                                  [600., 601., 602., 603., 604., 605., 606., 607., 608., 609., 610.],
+                                                                  [600., 601., 602., 603., 604., 605., 606., 607., 608., 609., 610.],
+                                                                  [600., 601., 602., 603., 604., 605., 606., 607., 608., 609., 610.],
+                                                                  [600., 601., 602., 603., 604., 605., 606., 607., 608., 609., 610.]]),
+                                'Motion Trajectory Calculation Fail: Rastered Path')
+        
+    def testStepsParsing_RasterEnergyPhase_Phase(self):
+        npt.assert_array_equal(self.egraster.phase_steps, ([[-45. , -45. , -45. , -45. , -45. , -45. , -45. , -45. , -45. ,  -45. , -45. ],
+                                                         [-22.5, -22.5, -22.5, -22.5, -22.5, -22.5, -22.5, -22.5, -22.5,  -22.5, -22.5],
+                                                         [  0. ,   0. ,   0. ,   0. ,   0. ,   0. ,   0. ,   0. ,   0. ,  0. ,   0. ],
+                                                         [ 22.5,  22.5,  22.5,  22.5,  22.5,  22.5,  22.5,  22.5,  22.5,  22.5,  22.5],
+                                                         [ 45. ,  45. ,  45. ,  45. ,  45. ,  45. ,  45. ,  45. ,  45. ,  45. ,  45. ]]),
+                                'Motion Trajectory Calculation Fail: Rastered Path')
+    
+            
     def testReadsGapShiftTableW(self):
         #self.eg.read_gap_shift_table()
-        self.assertIn('Harmonic 1', self.eg.gap_shift_table.keys(), 'input file is read incorrectly')
-        self.assertIn('Harmonic 3', self.eg.gap_shift_table.keys(),  'input file is read incorrectly')
-        self.assertIn('Harmonic 5', self.eg.gap_shift_table.keys(), 'input file is read incorrectly')
+        self.assertIn('Harmonic 1', self.gsep.gap_shift_table.keys(), 'input file is read incorrectly')
+        self.assertIn('Harmonic 3', self.gsep.gap_shift_table.keys(),  'input file is read incorrectly')
+        self.assertIn('Harmonic 5', self.gsep.gap_shift_table.keys(), 'input file is read incorrectly')
         
     def testReadGapShiftTableData(self):
-        self.assertEqual(self.eg.gap_shift_table['Harmonic 1'].__len__(), 8956, 'Input File reading incorrectly Harmonic 1')
-        self.assertEqual(self.eg.gap_shift_table['Harmonic 3'].__len__(), 16895, 'Input File reading incorrectly Harmonic 3')
-        self.assertEqual(self.eg.gap_shift_table['Harmonic 5'].__len__(), 18219, 'Input File reading incorrectly Harmonic 5')
+        self.assertEqual(self.gsep.gap_shift_table['Harmonic 1'].__len__(), 8952, 'Input File reading incorrectly Harmonic 1')
+        self.assertEqual(self.gsep.gap_shift_table['Harmonic 3'].__len__(), 16895, 'Input File reading incorrectly Harmonic 3')
+        self.assertEqual(self.gsep.gap_shift_table['Harmonic 5'].__len__(), 18219, 'Input File reading incorrectly Harmonic 5')
+        
+    def testCheckValidMotion(self):
+        self.eg.check_valid_motion()
+        npt.assert_array_equal(self.eg.allowed_harmonics, np.array([1.,3.]))
 
 
-#8956 Harmonic 1
-#16895 Harmonic 3
-#18219 Harmonic 5
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
