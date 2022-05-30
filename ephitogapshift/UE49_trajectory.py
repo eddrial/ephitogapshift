c9887d615fb2838f6d8ae79ba335046178b836a1
Ed Rial <eddrial@hotmail.com>
2022-05-30 16:24:36 +0200

    Saving functions and data in bin

--- ephitogapshift/UE49_trajectory.py
+++ ephitogapshift/UE49_trajectory.py
@@ -17,13 +17,17 @@ if __name__ == '__main__':
     parser.add_argument('-E','--Emax', type=float, required=True)
     parser.add_argument('-p','--Phimin', type=float, required=True)
     parser.add_argument('-P','--Phimax', type=float, required=True)
-    parser.add_argument('-s','--steps', type=int, required=True)
+    parser.add_argument('-s','--steps', nargs = '+', type=int, required=True)
     parser.add_argument('-o','--output', type=str, required=False)
     # Parse the argument
     args = parser.parse_args()# Print "Hello" + the user input argument
     
     print('loading data from ../bin/UE49_gapshift.idt')
     gapshift_2_Ephi = ephitogapshift.GapShiftEnergyPhase('../bin/UE49_gapshift.idt')
+    
+    #temp load
+    import pickle
+    
     print('creating reverse lookup table. This could take some time.')
     gapshift_2_Ephi.create_inverse_lookups()
     
@@ -40,9 +44,22 @@ if __name__ == '__main__':
     else:
         egraster = ephitogapshift.UndulatorMotion([args.Emin,args.Emax],[args.Phimin,args.Phimax],gapshift_2_Ephi,args.steps)
         
+        
+        
     egraster.create_motion_trajectory()
     egraster.check_valid_motion()
+    egraster.create_lookup_table()
     
     print('saving output')
-    egraster.save_motion_txt()
+
+    pname = 'C:/Users/oqb/git/ephitogapshift/bin'
+    fname  = 'UE49_1701e37p'
+    egraster.save_lookup_h5(pname,fname)
+    egraster.save_motion_txt()    
+    egraster.save_lookup_txt(pname, fname)
+    egraster.save_lookup_json(pname, fname)
+    egraster.save_lookup_pickle(pname, fname)
+    egraster.save_lookup_h5(pname, fname)
+    
+    #'C:/Users/oqb/git/ephitogapshift/bin','UE49'
     
--- ephitogapshift/ephitogapshift.py
+++ ephitogapshift/ephitogapshift.py
@@ -7,6 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.tri import Triangulation,  CubicTriInterpolator, TriInterpolator, TriAnalyzer
 
+import json
+import pickle
+import h5py as h5
+from mpmath import harmonic
+
 class UndulatorMotion(object):
     '''
     classdocs
@@ -20,6 +25,8 @@ class UndulatorMotion(object):
         self.steps = np.array([Steps]).flatten()
         self.phase = np.array([Phase]).flatten()
         self.energy = np.array([Energy]).flatten()
+        self.reqd_gaps = {}
+        self.reqd_shifts = {}
         self.motion = {}
         self.GSEP = GSEP
         
@@ -78,17 +85,48 @@ class UndulatorMotion(object):
         for harmonic in self.GSEP.gap_shift_table:
             #calculate gap and shift positions for energy-phase raster
             #en, ph = np.meshgrid(self.energy, self.phase)
-            reqd_gaps = self.GSEP.inverse_lookup['{} Gap'.format(harmonic)](self.energy_steps, self.phase_steps)
-            reqd_shifts = self.GSEP.inverse_lookup['{} Shift'.format(harmonic)](self.energy_steps, self.phase_steps)
+            self.reqd_gaps['{} Gap'.format(harmonic)] = self.GSEP.inverse_lookup['{} Gap'.format(harmonic)](self.energy_steps, self.phase_steps)
+            self.reqd_shifts['{} Shift'.format(harmonic)] = self.GSEP.inverse_lookup['{} Shift'.format(harmonic)](self.energy_steps, self.phase_steps)
                     
-            if np.isnan(reqd_gaps.data).any() or np.isnan(reqd_shifts.data).any():
+            if np.isnan(self.reqd_gaps['{} Gap'.format(harmonic)].data).any() or np.isnan(self.reqd_shifts['{} Shift'.format(harmonic)].data).any():
                 pass
             else:
                 self.allowed_harmonics = np.append(self.allowed_harmonics, int(harmonic[-1]))#append available harmonic
                 #store gap/shift pairs
-                self.motion[harmonic] = np.vstack((reqd_gaps.flatten(),reqd_shifts.flatten()))
+                self.motion[harmonic] = np.vstack((self.reqd_gaps['{} Gap'.format(harmonic)].flatten(),self.reqd_shifts['{} Shift'.format(harmonic)].flatten()))
                 
-                
+    
+    def create_lookup_table(self):
+        #I think this can be sliced together in a more intelligent way
+        #table is [Energy, Polarisation, Polarisation Mode, Harmonic,Undulator Position(Gap,Shift)]
+        all_harmonics = np.array([])
+        
+        for harmonic in self.GSEP.gap_shift_table:
+            #nptst[3,:,:,:,2,:] = int(harmonic[9:])
+            all_harmonics = np.append(all_harmonics, int(harmonic[9:]))
+            
+        self.lookup_table= np.meshgrid(self.energy_steps[0], self.phase_steps[:,0],np.linspace(0,1,1),all_harmonics,np.arange(5,7))
+        self.lookup_table = np.array(self.lookup_table)
+        
+        for h in range(len(all_harmonics)):
+            self.lookup_table[4,:,:,0,h,0]=self.reqd_gaps['Harmonic {} Gap'.format(int(self.lookup_table[3,0,0,0,h,0]))].data
+            self.lookup_table[4,:,:,0,h,1]=self.reqd_shifts['Harmonic {} Shift'.format(int(self.lookup_table[3,0,0,0,h,0]))].data 
+            
+        #create lookup_dictionary
+        self.lookup_dict = {}
+        for mode in range(len(self.lookup_table[0,0,0,:,0,0])):
+            self.lookup_dict[self.lookup_table[2,0,0,mode,0,0]] = {}
+            for harmonic in range(len(self.lookup_table[0,0,0,0,:,0])):
+                self.lookup_dict[self.lookup_table[2,0,0,mode,0,0]][self.lookup_table[3,0,0,mode,harmonic,0]] = {}
+                for polarisation in range(len(self.lookup_table[0,:,0,0,0,0])):
+                    self.lookup_dict[self.lookup_table[2,0,0,mode,0,0]][self.lookup_table[3,0,0,mode,harmonic,0]][self.lookup_table[1,polarisation,0,mode,harmonic,0]] = {}
+                    for energy in range(len(self.lookup_table[0,0,:,0,0,0])):
+                        self.lookup_dict[self.lookup_table[2,0,0,mode,0,0]][self.lookup_table[3,0,0,mode,harmonic,0]][self.lookup_table[1,polarisation,0,mode,harmonic,0]][self.lookup_table[0,polarisation,energy,mode,harmonic,0]] = self.lookup_table[4,polarisation,energy,mode,harmonic,:].tolist()
+                        
+                        
+        
+        
+    
     def save_motion_txt(self):
         #my_tab_header = ['#{%1s}{%10s}{%10s}{%10s}'.format('Energy', 'Poln', 'Gap', 'Shift')]
         
@@ -97,6 +135,60 @@ class UndulatorMotion(object):
             enphasegapshift_table = np.vstack([enphase_table,self.motion['Harmonic {}'.format(int(harmonic))]])
             np.savetxt('motiontst_Harmonic_{}.txt'.format(harmonic), enphasegapshift_table.T, fmt = ['%10.2f','%10.2f','%10.5f','%10.5f'])
     
+    def save_lookup_txt(self,mypath,myfname):
+        np.savetxt('{}/{}.icv.txt'.format(mypath,myfname),self.lookup_table.flatten(), delimiter = '\t', newline = '\n', 
+                   header = 'The correct array shape is \n{}\n[Axis, Energy, Polarisation, Polarisation Mode, Harmonic, Position [Gap, Shift]]'.format(str(self.lookup_table.shape)))
+    
+    def save_lookup_json(self, mypath, myfname):
+        with open('{}/{}_list.json'.format(mypath,myfname), 'w') as file:
+            json.dump(self.lookup_table.tolist(),file, indent = '\t')
+            
+        with open('{}/{}_dict.json'.format(mypath,myfname), 'w') as file:
+            json.dump(self.lookup_dict,file, indent = '\t')
+    
+    def save_lookup_pickle(self, mypath, myfname):
+        pickle.dump( self.lookup_table, open( '{}/{}_table.pkl'.format(mypath,myfname), "wb" ) )
+        pickle.dump( self.lookup_dict, open( '{}/{}_dict.pkl'.format(mypath,myfname), "wb" ) )
+
+    
+    def save_lookup_h5(self, mypath, myfname):
+        with h5.File('{}/{}.h5'.format(mypath, myfname), 'w') as h5f:
+            
+            und_table = h5f.create_dataset('undulator_table', data = self.lookup_table[4])
+            
+            energy = h5f.create_dataset('energy', data = self.lookup_table[0,0,:,0,0,0])
+            energy.make_scale('energy')
+            energy.attrs['units'] = 'eV'
+            und_table.dims[1].label = 'Photon Energy'
+            und_table.dims[1].attach_scale(energy)
+            
+            polarisation = h5f.create_dataset('polarisation', data = self.lookup_table[1,:,0,0,0,0])
+            polarisation.make_scale('polarisation')
+            polarisation.attrs['units'] = 'degrees'
+            und_table.dims[0].label = 'Polarisation Angle'
+            und_table.dims[0].attach_scale(polarisation)
+            
+            mode = h5f.create_dataset('mode', data = [s.encode() for s in ["Linear_Polarisation"]])
+            mode.make_scale('mode')
+            und_table.dims[2].label = 'Polarisation Mode'
+            und_table.dims[2].attach_scale(mode)
+            
+            und_mode = h5f.create_dataset('undulator mode', data = [s.encode() for s in ["Anti_Parallel"]])
+            und_mode.make_scale('undulator mode')
+            und_table.dims[2].label = 'Undulator Mode'
+            und_table.dims[2].attach_scale(und_mode)
+            
+            harmonic = h5f.create_dataset('harmonic', data = self.lookup_table[3,0,0,0,:,0])
+            harmonic.make_scale('harmonic')
+            und_table.dims[3].label = 'Harmonic'
+            und_table.dims[3].attach_scale(harmonic)
+            
+            und_coords = h5f.create_dataset('undulator position', data = [s.encode() for s in ["Gap", "Shift"]])
+            und_coords.make_scale('undulator position')
+            und_table.dims[4].label = 'Undulator Position'
+            und_table.dims[4].attach_scale(und_coords)
+            
+            
 class GapShiftEnergyPhase(object):
     
     def __init__(self, table_fname= '../bin/ba_file.idt'):
